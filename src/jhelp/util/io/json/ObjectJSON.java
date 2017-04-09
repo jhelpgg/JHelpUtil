ff25e49f8abb7c42c55f143b26caed5eaae0a728
JHelpgg <jhelpgg@gmail.com>
2017-04-09 18:17:35 +0200

    Find file/directory by path in ResourcesSystem | Add convenient methods in JSON | Better algorithm for compute components sizes | New Table layout | new JHelpTable easy to unify/split/move cells and action texts goes with | Correct a constructor in GenericAction | Add utilities for GenericAction | Add decoration and language descriptor to simplify setting JHelpAutoStyledTextArea and add some defaults

--- src/jhelp/util/io/json/ObjectJSON.java
+++ src/jhelp/util/io/json/ObjectJSON.java
@@ -9,12 +9,14 @@
 
 /*
  * License :
- * The following code is deliver as is. I take care that code compile and work, but I am not responsible about any damage it may cause.
+ * The following code is deliver as is. I take care that code compile and work, but I am not responsible about any
+ * damage it may cause.
  * You can use, modify, the code as your need for any usage.
  * But you can't do any action that avoid me or other person use, modify this code.
  * The code is free for usage and modification, you can't change that fact.
  * JHelp
  */
+
 package jhelp.util.io.json;
 
 import java.io.BufferedReader;
